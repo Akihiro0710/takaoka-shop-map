28a558ef502a028528115d45e9f55ab60ea506b1
akihiro0710 <acedia2074@gmail.com>
2018-11-08 07:50:57 +0900

    店舗のmigrationを作成

---
+++ database/migrations/2018_11_07_222840_create_shops_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('name');
+            $table->text('summary');
+            $table->text('business_hours');
+            $table->text('tel');
+            $table->text('address');
+            $table->double('lat');
+            $table->double('lon');
+            $table->text('eyecatch');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}
